2a2ae261b1d6d746461d31adecc4d47dbc56a546
Tal Hayut <tal.hayut@applitools.com>
2019-12-25 11:51:19 +0200

    fix: local modules

--- shellfie.js
+++ shellfie.js
@@ -28,7 +28,7 @@ async function shellfie(data, { name, location, style, theme = defaultTheme, ext
         const lines = Array.isArray(data) ? data : data.split('\n');
 
         // inject js scripts
-        const localModules = path.join(__dirname, 'node_modules')
+        const localModules = path.join(localPath, 'node_modules')
         await page.addScriptTag({ path: `${localModules}/xterm/lib/xterm.js` });
 
         // set page html
